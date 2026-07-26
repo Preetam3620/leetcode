122b45fc13863677048328580392c97e7e334bed
Preetam Maske <55644679+Preetam3620@users.noreply.github.com>
2026-07-25 23:05:55 -0700

    Time: 4 ms (15.16%) | Memory: 19.2 MB (87.21%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -7,13 +7,10 @@ class Solution:
         start = 0
         for i in range(1, len(chars)):
             if chars[i] != chars[i - 1]:
-                if i - start == 1:
-                    result.append(chars[i - 1])
+                result.append(chars[i - 1])
                 if i - start > 1:
-                    result.append(chars[i - 1])
                     result.append(str(i - start))
                 start = i
-            # print(start)
 
         result.append(chars[start])
         if len(chars) - start > 1:
