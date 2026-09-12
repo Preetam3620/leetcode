bad43d90100490aa876d9c50fc31347b70d8f338
Preetam Maske <55644679+Preetam3620@users.noreply.github.com>
2026-09-11 20:30:44 -0700

    Time: 19 ms (19.68%) | Memory: 24 MB (17.30%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = []
-        hashMap = {}
+        hashMap = defaultdict(list)
         for word in strs:
             count = [0] * 26
             for char in word:
                 count[ord(char) - ord("a")] += 1
-            if tuple(count) not in hashMap:
-                hashMap[tuple(count)] = [word]
-            else:
-                hashMap[tuple(count)].append(word)
+            
+            hashMap[tuple(count)].append(word)
         
-        result = list(hashMap.values())
-        return result
+        return list(hashMap.values())
