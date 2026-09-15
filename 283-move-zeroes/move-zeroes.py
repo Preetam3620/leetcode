e75220404c476fb45b3b4dbcd10f7bef3c3d007f
Preetam Maske <55644679+Preetam3620@users.noreply.github.com>
2026-09-14 18:36:50 -0700

    Time: 3 ms (81.85%) | Memory: 20.7 MB (5.21%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,26 +5,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # zeroPointer = -1
-        # curr = 0
-        # while curr < len(nums):
-        #     if nums[curr] == 0:
-        #         zeroPointer = curr
-        #     else:
-        #         if zeroPointer == curr:
-        #             curr += 1
-        #         else:
-        #             temp = nums[curr]
-        #             nums[curr] = nums[zeroPointer]
-        #             nums[zeroPointer] = temp
-        #     print(nums)
-        arr = [0] * len(nums)
-        k = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                arr[k] = nums[i]
-                k += 1
-        
-        for i in range(len(arr)):
-            nums[i] = arr[i]
-        print(nums, arr)
+        nonZeroIndex = 0
+        for curr in range(len(nums)):
+            if nums[curr] != 0:
+                nums[curr], nums[nonZeroIndex] = nums[nonZeroIndex], nums[curr]
+                nonZeroIndex += 1
