f6a290e57b6732a8b603886276863f4e5bb6a6d0
Preetam Maske <55644679+Preetam3620@users.noreply.github.com>
2026-09-20 23:49:51 -0700

    Time: 24 ms (19.41%) | Memory: 22.2 MB (99.86%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,16 +1,14 @@
 class Solution:
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trust_given = [0] * (n + 1)   # how many people this person trusts
-        trust_received = [0] * (n + 1) # how many people trust this person
+    def findJudge(self, n: int, trust: list[list[int]]) -> int:
+        incoming = defaultdict(int)
+        outgoing = defaultdict(int)
 
-        for a, b in trust:
-            trust_given[a] += 1
-            trust_received[b] += 1
-
-        print(trust_given, trust_received)
+        for (k, v) in trust:
+            incoming[v] += 1
+            outgoing[k] += 1
 
         for i in range(1, n + 1):
-            if trust_given[i] == 0 and trust_received[i] == n - 1:
+            if incoming[i] == (n - 1) and outgoing[i] == 0:
                 return i
-
+        
         return -1
